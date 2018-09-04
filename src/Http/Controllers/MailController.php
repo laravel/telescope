3e3536deedbd2d95527629d835a713e0767f37da
Taylor Otwell <taylor@laravel.com>
2018-09-04 15:50:18 -0500

    wip

--- src/Http/Controllers/MailController.php
+++ src/Http/Controllers/MailController.php
@@ -10,24 +10,6 @@ use Laravel\Telescope\Contracts\EntriesRepository;
 class MailController extends EntryController
 {
     /**
-     * Preview the HTML content of the email.
-     *
-     * @param  \Illuminate\Http\Request $request
-     * @param  \Laravel\Telescope\Contracts\EntriesRepository $storage
-     * @param  integer $id
-     * @return mixed
-     */
-    public function downloadEml(Request $request, EntriesRepository $storage, $id)
-    {
-        $mail = $storage->find($id);
-
-        return response($mail->content->raw, 200, [
-            'Content-Type' => 'message/rfc822',
-            'Content-Disposition' => 'attachment; filename="mail-'.$id.'.eml"',
-        ]);
-    }
-
-    /**
      * The entry type for the controller.
      *
      * @return int
