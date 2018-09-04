70b3eeacf160849ab88629021b10457f48ee1244
Taylor Otwell <taylor@laravel.com>
2018-09-04 15:50:23 -0500

    wip

--- src/Http/Controllers/MailEmlController.php
+++ src/Http/Controllers/MailEmlController.php
@@ -3,7 +3,6 @@
 namespace Laravel\Telescope\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Laravel\Telescope\EntryType;
 use Illuminate\Routing\Controller;
 use Laravel\Telescope\Contracts\EntriesRepository;
 
