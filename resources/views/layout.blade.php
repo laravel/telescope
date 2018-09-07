9248c1bf37603bf0d7e3dbb0a16be4e77632ead3
Mohamed Said <themohamedsaid@gmail.com>
2018-09-07 15:17:41 +0200

    wip

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -117,7 +117,7 @@
                     <li class="nav-item">
                         <router-link active-class="active" to="/schedule" class="nav-link d-flex align-items-center">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                <path d="M1 1h18v2H1V1zm0 8h18v2H1V9zm0 8h18v2H1v-2zM1 5h12v2H1V5zm0 8h12v2H1v-2z"/>
+                                <path d="M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm-1-7.59V4h2v5.59l3.95 3.95-1.41 1.41L9 10.41z"/>
                             </svg>
                             <span>Schedule</span>
                         </router-link>
