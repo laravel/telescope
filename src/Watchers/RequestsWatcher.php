5efe669680aed03004c8075edd5995e0fa99c75b
Taylor Otwell <taylor@laravel.com>
2018-09-07 15:33:30 -0500

    wip

--- src/Watchers/RequestsWatcher.php
+++ src/Watchers/RequestsWatcher.php
@@ -30,13 +30,26 @@ class RequestsWatcher extends Watcher
     public function recordRequest(RequestHandled $event)
     {
         Telescope::recordRequest(IncomingEntry::make([
-            'payload' => $this->formatPayload($event->request->all()),
             'uri' => str_replace($event->request->root(), '', $event->request->path()),
             'method' => $event->request->method(),
             'headers' => $this->formatHeaders($event->request->headers->all()),
+            'payload' => $this->formatPayload($event->request->all()),
             'response' => $this->formatResponse($event->response),
             'response_status' => $event->response->getStatusCode(),
         ]));
+    }
+
+    /**
+     * Format the given headers.
+     *
+     * @param  array $headers
+     * @return array
+     */
+    protected function formatHeaders($headers)
+    {
+        return collect($headers)->map(function ($header) {
+            return $header[0];
+        })->toArray();
     }
 
     /**
@@ -45,7 +58,7 @@ class RequestsWatcher extends Watcher
      * @param  array $payload
      * @return array
      */
-    private function formatPayload($payload)
+    protected function formatPayload($payload)
     {
         foreach (Telescope::$protectedRequestParameters as $parameter) {
             if (Arr::get($payload, $parameter)) {
@@ -57,25 +70,12 @@ class RequestsWatcher extends Watcher
     }
 
     /**
-     * Format the given headers.
-     *
-     * @param  array $headers
-     * @return array
-     */
-    private function formatHeaders($headers)
-    {
-        return collect($headers)->map(function ($header) {
-            return $header[0];
-        })->toArray();
-    }
-
-    /**
      * Format the given response object.
      *
      * @param  \Symfony\Component\HttpFoundation\Response $response
      * @return array
      */
-    private function formatResponse(Response $response)
+    protected function formatResponse(Response $response)
     {
         if (is_string($response->getContent()) &&
             is_array(json_decode($response->getContent(), true)) &&
