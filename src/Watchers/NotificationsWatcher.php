eab52e78e31be25395df42ef00ffa034f7f3932f
Taylor Otwell <taylor@laravel.com>
2018-09-07 15:20:15 -0500

    wip

--- src/Watchers/NotificationsWatcher.php
+++ src/Watchers/NotificationsWatcher.php
@@ -37,32 +37,6 @@ class NotificationsWatcher extends Watcher
     }
 
     /**
-     * Format the given notifiable.
-     *
-     * @param  mixed $notifiable
-     * @return string
-     */
-    private function formatNotifiable($notifiable)
-    {
-        if ($notifiable instanceof Model) {
-            return get_class($notifiable).':'.$notifiable->getKey();
-        }
-
-        return $notifiable;
-    }
-
-    /**
-     * Format the given notification.
-     *
-     * @param  \Illuminate\Notifications\Notification $notification
-     * @return string
-     */
-    private function formatNotification($notification)
-    {
-        return get_class($notification);
-    }
-
-    /**
      * Extract tags from the given event.
      *
      * @param  \Illuminate\Notifications\Events\NotificationSent $event
@@ -70,6 +44,22 @@ class NotificationsWatcher extends Watcher
      */
     private function extractTagsFromEvent($event)
     {
-        return [$this->formatNotification($event->notification), $this->formatNotifiable($event->notifiable)];
+        return [
+            get_class($event->notification),
+            $this->formatNotifiable($event->notifiable)
+        ];
+    }
+
+    /**
+     * Format the given notifiable.
+     *
+     * @param  mixed  $notifiable
+     * @return string
+     */
+    private function formatNotifiable($notifiable)
+    {
+        return $notifiable instanceof Model
+                ? get_class($notifiable).':'.$notifiable->getKey()
+                : $notifiable;
     }
 }
