f98ca0741b862d9de99b041c73ccac9a7e1d2c18
Taylor Otwell <taylor@laravel.com>
2018-09-10 13:04:43 -0500

    wip

---
+++ config/telescope.php
@@ -0,0 +1,64 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Telescope Watchers
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure the configurations for each watcher that is
+    | used by Telescope.
+    |
+    */
+
+    'watchers' => [
+        'cache' => [
+            'enabled' => true,
+        ],
+
+        'events' => [
+            'enabled' => true,
+        ],
+
+        'exceptions' => [
+            'enabled' => true,
+        ],
+
+        'logs' => [
+            'enabled' => true,
+        ],
+
+        'mail' => [
+            'enabled' => true,
+        ],
+
+        'notifications' => [
+            'enabled' => true,
+        ],
+
+        'queue' => [
+            'enabled' => true,
+        ],
+
+        'queries' => [
+            'enabled' => true,
+        ],
+
+        'requests' => [
+            'enabled' => true,
+        ],
+
+        'artisan' => [
+            'enabled' => true,
+        ],
+
+        'schedule' => [
+            'enabled' => true,
+        ],
+
+        'redis' => [
+            'enabled' => true,
+        ],
+    ],
+];
