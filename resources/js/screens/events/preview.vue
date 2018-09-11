3c87589d5e1ccbe8ad59b0217335995a13640cb0
Taylor Otwell <taylor@laravel.com>
2018-09-11 16:07:54 -0500

    wip

--- resources/js/screens/events/preview.vue
+++ resources/js/screens/events/preview.vue
@@ -21,12 +21,14 @@
         </template>
 
         <div slot="after-attributes-card" slot-scope="slotProps">
+            <!-- Event Payload -->
             <div class="card mt-5" v-if="slotProps.entry.content.payload">
                 <div class="card-header"><h5>Event Data</h5></div>
 
                 <pre class="bg-dark p-4 mb-0 text-white"><tree-view :data="slotProps.entry.content.payload" :options="{maxDepth: 3}"></tree-view></pre>
             </div>
 
+            <!-- Event Listeners -->
             <div class="card mt-5" v-if="slotProps.entry.content.listeners && slotProps.entry.content.listeners.length">
                 <div class="card-header"><h5>Registered Listeners</h5></div>
 
