21e30455284e193135a63bbb1b527664be83b893
Taylor Otwell <taylor@laravel.com>
2018-09-12 19:46:39 -0500

    wip

--- src/TelescopeServiceProvider.php
+++ src/TelescopeServiceProvider.php
@@ -154,19 +154,19 @@ class TelescopeServiceProvider extends ServiceProvider
     private function registerWatchers()
     {
         $watchers = [
+            Watchers\CacheWatcher::class => config('telescope.watchers.cache.enabled'),
+            Watchers\CommandWatcher::class => config('telescope.watchers.commands.enabled'),
+            Watchers\EventWatcher::class => config('telescope.watchers.events.enabled'),
             Watchers\ExceptionWatcher::class => config('telescope.watchers.exceptions.enabled'),
+            Watchers\JobWatcher::class => config('telescope.watchers.jobs.enabled'),
             Watchers\LogWatcher::class => config('telescope.watchers.logs.enabled'),
             Watchers\MailWatcher::class => config('telescope.watchers.mail.enabled'),
-            Watchers\JobWatcher::class => config('telescope.watchers.jobs.enabled'),
-            Watchers\CacheWatcher::class => config('telescope.watchers.cache.enabled'),
-            Watchers\EventWatcher::class => config('telescope.watchers.events.enabled'),
+            Watchers\ModelWatcher::class => config('telescope.watchers.models.enabled'),
             Watchers\NotificationWatcher::class => config('telescope.watchers.notifications.enabled'),
             Watchers\QueryWatcher::class => config('telescope.watchers.queries.enabled'),
-            Watchers\RequestWatcher::class => config('telescope.watchers.requests.enabled'),
-            Watchers\CommandWatcher::class => config('telescope.watchers.commands.enabled'),
-            Watchers\ScheduleWatcher::class => config('telescope.watchers.schedule.enabled'),
             Watchers\RedisWatcher::class => config('telescope.watchers.redis.enabled'),
-            Watchers\ModelWatcher::class => config('telescope.watchers.models.enabled'),
+            Watchers\RequestWatcher::class => config('telescope.watchers.requests.enabled'),
+            Watchers\ScheduleWatcher::class => config('telescope.watchers.schedule.enabled'),
         ];
 
         foreach (array_keys(array_filter($watchers)) as $watcher) {
