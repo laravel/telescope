be9af9a1f580f14f4c29932e1597df7c70d34859
Taylor Otwell <taylor@laravel.com>
2018-09-14 10:54:35 -0500

    wip

---
+++ src/Http/Controllers/MonitoredTagController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Laravel\Telescope\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Laravel\Telescope\Contracts\EntriesRepository;
+
+class MonitoredTagController extends Controller
+{
+    /**
+     * The entry repository implementation.
+     *
+     * @var \Laravel\Telescope\Contracts\EntriesRepository
+     */
+    protected $entries;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @param  \Laravel\Telescope\Contracts\EntriesRepository  $entries
+     * @return void
+     */
+    public function __construct(EntriesRepository $entries)
+    {
+        $this->entries = $entries;
+    }
+
+    /**
+     * Get all of the tags being monitored.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        return response()->json($this->entries->monitoring());
+    }
+
+    /**
+     * Begin monitoring the given tag.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        $this->entries->monitor([$request->tag]);
+    }
+
+    /**
+     * Stop monitoring the given tag.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return Response
+     */
+    public function destroy(Request $request)
+    {
+        $this->entries->stopMonitoring([$request->tag]);
+    }
+}
