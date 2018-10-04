3dd19a043991217489a2b6827996018cdefdedd0
Mohamed Said <themohamedsaid@gmail.com>
2018-10-04 14:20:43 +0200

    order

--- src/EntryType.php
+++ src/EntryType.php
@@ -11,11 +11,11 @@ class EntryType
     public const EXCEPTION = 'exception';
     public const JOB = 'job';
     public const LOG = 'log';
+    public const MODEL = 'model';
     public const MAIL = 'mail';
     public const NOTIFICATION = 'notification';
     public const QUERY = 'query';
+    public const REDIS = 'redis';
     public const REQUEST = 'request';
     public const SCHEDULED_TASK = 'schedule';
-    public const REDIS = 'redis';
-    public const MODEL = 'model';
 }
