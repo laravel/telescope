e24c55a859d7bac85643ffde04053b72ceeb48a9
Taylor Otwell <taylor@laravel.com>
2018-10-04 12:41:13 -0500

    wip

--- public/app.js
+++ public/app.js
@@ -63148,9 +63148,32 @@ var render = function() {
           "div",
           {
             staticClass:
-              "d-flex align-items-center justify-content-center card-bg-secondary p-5 bottom-radius"
+              "d-flex flex-column align-items-center justify-content-center card-bg-secondary p-5 bottom-radius"
           },
-          [_c("span", [_vm._v("No entries found.")])]
+          [
+            _c(
+              "svg",
+              {
+                staticClass: "fill-text-color",
+                staticStyle: { width: "200px" },
+                attrs: {
+                  xmlns: "http://www.w3.org/2000/svg",
+                  viewBox: "0 0 60 60"
+                }
+              },
+              [
+                _c("path", {
+                  attrs: {
+                    "fill-rule": "evenodd",
+                    d:
+                      "M7 10h41a11 11 0 0 1 0 22h-8a3 3 0 0 0 0 6h6a6 6 0 1 1 0 12H10a4 4 0 1 1 0-8h2a2 2 0 1 0 0-4H7a5 5 0 0 1 0-10h3a3 3 0 0 0 0-6H7a6 6 0 1 1 0-12zm14 19a1 1 0 0 1-1-1 1 1 0 0 0-2 0 1 1 0 0 1-1 1 1 1 0 0 0 0 2 1 1 0 0 1 1 1 1 1 0 0 0 2 0 1 1 0 0 1 1-1 1 1 0 0 0 0-2zm-5.5-11a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zm24 10a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zm1 18a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zm-14-3a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zm22-23a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zM33 18a1 1 0 0 1-1-1v-1a1 1 0 0 0-2 0v1a1 1 0 0 1-1 1h-1a1 1 0 0 0 0 2h1a1 1 0 0 1 1 1v1a1 1 0 0 0 2 0v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 0-2h-1z"
+                  }
+                })
+              ]
+            ),
+            _vm._v(" "),
+            _c("span", [_vm._v("We didn't find anything - just empty space.")])
+          ]
         )
       : _vm._e(),
     _vm._v(" "),
@@ -79534,7 +79557,7 @@ var render = function() {
               ]
             ),
             _vm._v(" "),
-            _c("span", [_vm._v("No entries found.")])
+            _c("span", [_vm._v("We didn't find anything - just empty space.")])
           ]
         )
       : _vm._e(),
