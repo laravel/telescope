003b51993902faad97f2f4fcf1ffc85bfe82d837
Yohanan Baruchel <ybaruchel@gmail.com>
2018-10-23 23:44:25 +0300

    Fix indentations

--- src/Telescope.php
+++ src/Telescope.php
@@ -558,7 +558,7 @@ class Telescope
     {
         return [
             'path' => config('telescope.path'),
-			'timezone' => config('app.timezone')
-		];
+            'timezone' => config('app.timezone')
+        ];
     }
 }
