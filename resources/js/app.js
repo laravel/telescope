1c636605593dfe9f0693b63e546066258ea0ca02
Taylor Otwell <taylor@laravel.com>
2018-10-26 14:51:24 -0500

    formatting

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,15 @@
 import Vue from 'vue'
 import Base from './base'
+import axios from 'axios';
 import Routes from './routes'
 import VueRouter from 'vue-router'
 import TreeView from 'vue-json-tree-view'
 import moment from 'moment-timezone';
-import axios from 'axios';
 
 require('bootstrap');
 
 let token = document.head.querySelector('meta[name="csrf-token"]');
+
 if (token) {
     axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
 }
