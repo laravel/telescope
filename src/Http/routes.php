010896bbafb1113551afa30f5aebc184a73d161b
Mohamed Said <themohamedsaid@gmail.com>
2018-10-29 14:27:05 +0200

    wip

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -2,60 +2,60 @@
 
 // Mail entries...
 Route::post('/telescope-api/mail', 'MailController@index');
-Route::get('/telescope-api/mail/{uuid}', 'MailController@show');
-Route::get('/telescope-api/mail/{uuid}/preview', 'MailHtmlController@show');
-Route::get('/telescope-api/mail/{uuid}/download', 'MailEmlController@show');
+Route::get('/telescope-api/mail/{telescopeEntryId}', 'MailController@show');
+Route::get('/telescope-api/mail/{telescopeEntryId}/preview', 'MailHtmlController@show');
+Route::get('/telescope-api/mail/{telescopeEntryId}/download', 'MailEmlController@show');
 
 // Exception entries...
 Route::post('/telescope-api/exceptions', 'ExceptionController@index');
-Route::get('/telescope-api/exceptions/{uuid}', 'ExceptionController@show');
+Route::get('/telescope-api/exceptions/{telescopeEntryId}', 'ExceptionController@show');
 
 // Dump entries...
 Route::post('/telescope-api/dumps', 'DumpController@index');
 
 // Log entries...
 Route::post('/telescope-api/logs', 'LogController@index');
-Route::get('/telescope-api/logs/{uuid}', 'LogController@show');
+Route::get('/telescope-api/logs/{telescopeEntryId}', 'LogController@show');
 
 // Notifications entries...
 Route::post('/telescope-api/notifications', 'NotificationsController@index');
-Route::get('/telescope-api/notifications/{uuid}', 'NotificationsController@show');
+Route::get('/telescope-api/notifications/{telescopeEntryId}', 'NotificationsController@show');
 
 // Queue entries...
 Route::post('/telescope-api/jobs', 'QueueController@index');
-Route::get('/telescope-api/jobs/{uuid}', 'QueueController@show');
+Route::get('/telescope-api/jobs/{telescopeEntryId}', 'QueueController@show');
 
 // Events entries...
 Route::post('/telescope-api/events', 'EventsController@index');
-Route::get('/telescope-api/events/{uuid}', 'EventsController@show');
+Route::get('/telescope-api/events/{telescopeEntryId}', 'EventsController@show');
 
 // Cache entries...
 Route::post('/telescope-api/cache', 'CacheController@index');
-Route::get('/telescope-api/cache/{uuid}', 'CacheController@show');
+Route::get('/telescope-api/cache/{telescopeEntryId}', 'CacheController@show');
 
 // Queries entries...
 Route::post('/telescope-api/queries', 'QueriesController@index');
-Route::get('/telescope-api/queries/{uuid}', 'QueriesController@show');
+Route::get('/telescope-api/queries/{telescopeEntryId}', 'QueriesController@show');
 
 // Eloquent entries...
 Route::post('/telescope-api/models', 'ModelsController@index');
-Route::get('/telescope-api/models/{uuid}', 'ModelsController@show');
+Route::get('/telescope-api/models/{telescopeEntryId}', 'ModelsController@show');
 
 // Requests entries...
 Route::post('/telescope-api/requests', 'RequestsController@index');
-Route::get('/telescope-api/requests/{uuid}', 'RequestsController@show');
+Route::get('/telescope-api/requests/{telescopeEntryId}', 'RequestsController@show');
 
 // Artisan Commands entries...
 Route::post('/telescope-api/commands', 'CommandsController@index');
-Route::get('/telescope-api/commands/{uuid}', 'CommandsController@show');
+Route::get('/telescope-api/commands/{telescopeEntryId}', 'CommandsController@show');
 
 // Scheduled Commands entries...
 Route::post('/telescope-api/schedule', 'ScheduleController@index');
-Route::get('/telescope-api/schedule/{uuid}', 'ScheduleController@show');
+Route::get('/telescope-api/schedule/{telescopeEntryId}', 'ScheduleController@show');
 
 // Redis Commands entries...
 Route::post('/telescope-api/redis', 'RedisController@index');
-Route::get('/telescope-api/redis/{uuid}', 'RedisController@show');
+Route::get('/telescope-api/redis/{telescopeEntryId}', 'RedisController@show');
 
 // Monitored Tags...
 Route::get('/telescope-api/monitored-tags', 'MonitoredTagController@index');
