2d5b8e6be48c69a1512ef40ef6ce2b6790a393e3
Deleu <deleugyn@gmail.com>
2018-10-29 16:00:00 -0400

    Simple Log Watcher Test

---
+++ tests/Watchers/LogWatcherTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Laravel\Telescope\Tests\Watchers;
+
+use Psr\Log\LogLevel;
+use Psr\Log\LoggerInterface;
+use Laravel\Telescope\EntryType;
+use Laravel\Telescope\Watchers\LogWatcher;
+use Laravel\Telescope\Tests\FeatureTestCase;
+
+class LogWatcherTest extends FeatureTestCase
+{
+    protected function getEnvironmentSetUp($app)
+    {
+        parent::getEnvironmentSetUp($app);
+
+        $app->get('config')->set('logging.default', 'syslog');
+
+        $app->get('config')->set('telescope.watchers', [
+            LogWatcher::class => true,
+        ]);
+    }
+
+    public function logLevelProvider()
+    {
+        return [
+            [LogLevel::EMERGENCY],
+            [LogLevel::ALERT],
+            [LogLevel::CRITICAL],
+            [LogLevel::ERROR],
+            [LogLevel::WARNING],
+            [LogLevel::NOTICE],
+            [LogLevel::INFO],
+            [LogLevel::DEBUG],
+        ];
+    }
+
+    /**
+     * @dataProvider logLevelProvider
+     */
+    public function test_log_watcher_registers_entry_for_any_level($level)
+    {
+        $logger = $this->app->get(LoggerInterface::class);
+
+        $logger->$level("Logging Level [$level].", [
+            'user' => 'Claire Redfield',
+            'role' => 'Zombie Hunter'
+        ]);
+
+        $entry = $this->loadTelescopeEntries()->first();
+
+        self::assertSame(EntryType::LOG, $entry->type);
+        self::assertSame($level, $entry->content['level']);
+        self::assertSame("Logging Level [$level].", $entry->content['message']);
+        self::assertSame('Claire Redfield', $entry->content['context']['user']);
+        self::assertSame('Zombie Hunter', $entry->content['context']['role']);
+    }
+}
