ffb14742587f07c84a37106ab6f08eb58cccb5e0
Deleu <deleugyn@gmail.com>
2018-10-30 10:19:21 -0400

    Fix PruneCommand contract

--- src/TelescopeServiceProvider.php
+++ src/TelescopeServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Telescope\Contracts\ClearableRepository;
 use Laravel\Telescope\Contracts\EntriesRepository;
+use Laravel\Telescope\Contracts\PrunableRepository;
 use Laravel\Telescope\Storage\DatabaseEntriesRepository;
 
 class TelescopeServiceProvider extends ServiceProvider
@@ -138,6 +139,10 @@ class TelescopeServiceProvider extends ServiceProvider
 
         $this->app->singleton(
             ClearableRepository::class, DatabaseEntriesRepository::class
+        );
+
+        $this->app->singleton(
+            PrunableRepository::class, DatabaseEntriesRepository::class
         );
 
         $this->app->when(DatabaseEntriesRepository::class)
