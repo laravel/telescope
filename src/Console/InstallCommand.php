2a9c3917df6c21c19c99a42f040cf2748d04276b
LinJi <237525603@qq.com>
2018-10-31 10:46:11 +0800

    Update InstallCommand.php
    fix php artisan telescope:install repeat to add

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -56,7 +56,7 @@ class InstallCommand extends Command
 
         $appConfig = file_get_contents(config_path('app.php'));
 
-        if (Str::contains($appConfig, $namespace."Providers\\TelescopeServiceProvider::class")) {
+        if (Str::contains($appConfig, $namespace."\\Providers\\TelescopeServiceProvider::class")) {
             return;
         }
 
