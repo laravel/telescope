1407b852ad2d9cfe6178275949f1a0e44b8da14a
Paras Malhotra <paras@insenseanalytics.com>
2018-11-02 03:19:37 +0530

    Add simple Dump Watcher Test

---
+++ tests/Watchers/DumpWatcherTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Laravel\Telescope\Tests\Watchers;
+
+use Illuminate\Contracts\Cache\Repository;
+use Laravel\Telescope\EntryType;
+use Laravel\Telescope\Tests\FeatureTestCase;
+use Laravel\Telescope\Watchers\DumpWatcher;
+
+class DumpWatcherTest extends FeatureTestCase
+{
+    protected function getEnvironmentSetUp($app)
+    {
+        parent::getEnvironmentSetUp($app);
+
+        $app->make(Repository::class)->forever('telescope:dump-watcher', '1');
+
+        $app->get('config')->set('telescope.watchers', [
+            DumpWatcher::class => true,
+        ]);
+    }
+
+    public function test_dump_watcher_register_entry()
+    {
+        $var = 'Telescopes are better than binoculars';
+        dump($var);
+        
+        $entry = $this->loadTelescopeEntries()->first();
+
+        self::assertSame(EntryType::DUMP, $entry->type);
+        self::assertContains($var, $entry->content['dump']);
+    }
+}
