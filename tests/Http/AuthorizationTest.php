1515bb2417c01a0392f2571f0b32159b5f123a47
Deleu <deleugyn@gmail.com>
2018-11-04 04:57:58 -0500

    Test Authenticatable interface

--- tests/Http/AuthorizationTest.php
+++ tests/Http/AuthorizationTest.php
@@ -2,6 +2,8 @@
 
 namespace Laravel\Telescope\Tests\Http;
 
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Http\Request;
 use Laravel\Telescope\Telescope;
 use Illuminate\Support\Facades\Gate;
 use Laravel\Telescope\Tests\FeatureTestCase;
@@ -23,14 +25,34 @@ class AuthorizationTest extends FeatureTestCase
         Telescope::auth(null);
     }
 
-    public function test_unauthorized_by_gate()
+    public function test_guests_gets_unauthorized_by_gate()
     {
+        Telescope::auth(function (Request $request) {
+            return Gate::check('viewTelescope', [$request->user()]);
+        });
+
         Gate::define('viewTelescope', function ($user) {
-            return false;
+            return true;
         });
 
         $this->post('/telescope/telescope-api/requests')
             ->assertStatus(403);
+    }
+
+    public function test_authenticated_user_gets_authorized_by_gate()
+    {
+        $this->actingAs(new Authenticated);
+
+        Telescope::auth(function (Request $request) {
+            return Gate::check('viewTelescope', [$request->user()]);
+        });
+
+        Gate::define('viewTelescope', function (Authenticatable $user) {
+            return $user->getAuthIdentifier() == 'telescope-test';
+        });
+
+        $this->post('/telescope/telescope-api/requests')
+            ->assertStatus(200);
     }
 
     public function test_unauthorized_requests()
@@ -51,5 +73,38 @@ class AuthorizationTest extends FeatureTestCase
 
         $this->post('/telescope/telescope-api/requests')
             ->assertSuccessful();
+    }
+}
+
+class Authenticated implements Authenticatable
+{
+    public function getAuthIdentifierName()
+    {
+        return 'Telescope Test';
+    }
+
+    public function getAuthIdentifier()
+    {
+        return 'telescope-test';
+    }
+
+    public function getAuthPassword()
+    {
+        return 'secret';
+    }
+
+    public function getRememberToken()
+    {
+        return 'i-am-telescope';
+    }
+
+    public function setRememberToken($value)
+    {
+        //
+    }
+
+    public function getRememberTokenName()
+    {
+        //
     }
 }
