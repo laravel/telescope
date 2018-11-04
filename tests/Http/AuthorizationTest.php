f3e10427464d1c006ab9171d3ae29c385db47109
Deleu <deleugyn@gmail.com>
2018-11-04 04:29:31 -0500

    Test Unauthorized Requests

---
+++ tests/Http/AuthorizationTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Laravel\Telescope\Tests\Http;
+
+use Laravel\Telescope\Telescope;
+use Laravel\Telescope\Tests\FeatureTestCase;
+use Orchestra\Testbench\Http\Middleware\VerifyCsrfToken;
+
+class AuthorizationTest extends FeatureTestCase
+{
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->withoutMiddleware([VerifyCsrfToken::class]);
+    }
+
+    public function test_unauthorized_requests()
+    {
+        Telescope::auth(function () {
+            return false;
+        });
+
+        $this->get('/telescope/telescope-api/requests')
+            ->assertStatus(403);
+    }
+}
