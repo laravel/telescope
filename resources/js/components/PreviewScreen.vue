9bbb98e531a9600063b10aad1a2c543e3bc7b930
Mohamed Said <themohamedsaid@gmail.com>
2018-11-15 17:41:20 +0200

    make preview details table responsive

--- resources/js/components/PreviewScreen.vue
+++ resources/js/components/PreviewScreen.vue
@@ -157,65 +157,67 @@
             </div>
 
 
-            <table v-if="ready && entry" class="table mb-0 card-bg-secondary table-borderless">
-                <tbody>
-                <tr>
-                    <td class="table-fit font-weight-bold">Time</td>
-                    <td>
-                        {{localTime(entry.created_at)}} ({{timeAgo(entry.created_at)}})
-                    </td>
-                </tr>
+            <div class="table-responsive">
+                <table v-if="ready && entry" class="table mb-0 card-bg-secondary table-borderless">
+                    <tbody>
+                    <tr>
+                        <td class="table-fit font-weight-bold">Time</td>
+                        <td>
+                            {{localTime(entry.created_at)}} ({{timeAgo(entry.created_at)}})
+                        </td>
+                    </tr>
 
-                <tr>
-                    <td class="table-fit font-weight-bold">Hostname</td>
-                    <td>
-                        {{entry.content.hostname}}
-                    </td>
-                </tr>
+                    <tr>
+                        <td class="table-fit font-weight-bold">Hostname</td>
+                        <td>
+                            {{entry.content.hostname}}
+                        </td>
+                    </tr>
 
-                <slot name="table-parameters" :entry="entry"></slot>
+                    <slot name="table-parameters" :entry="entry"></slot>
 
-                <tr v-if="!entryPoint && job">
-                    <td class="table-fit font-weight-bold">Job</td>
-                    <td>
-                        <router-link :to="{name:'job-preview', params:{id: job.id}}" class="control-action">
-                            View Job
-                        </router-link>
-                    </td>
-                </tr>
+                    <tr v-if="!entryPoint && job">
+                        <td class="table-fit font-weight-bold">Job</td>
+                        <td>
+                            <router-link :to="{name:'job-preview', params:{id: job.id}}" class="control-action">
+                                View Job
+                            </router-link>
+                        </td>
+                    </tr>
 
-                <tr v-if="!entryPoint && request">
-                    <td class="table-fit font-weight-bold">Request</td>
-                    <td>
-                        <router-link :to="{name:'request-preview', params:{id: request.id}}" class="control-action">
-                            View Request
-                        </router-link>
-                    </td>
-                </tr>
+                    <tr v-if="!entryPoint && request">
+                        <td class="table-fit font-weight-bold">Request</td>
+                        <td>
+                            <router-link :to="{name:'request-preview', params:{id: request.id}}" class="control-action">
+                                View Request
+                            </router-link>
+                        </td>
+                    </tr>
 
-                <tr v-if="!entryPoint && command">
-                    <td class="table-fit font-weight-bold">Command</td>
-                    <td>
-                        <router-link :to="{name:'command-preview', params:{id: command.id}}" class="control-action">
-                            View Command
-                        </router-link>
-                    </td>
-                </tr>
+                    <tr v-if="!entryPoint && command">
+                        <td class="table-fit font-weight-bold">Command</td>
+                        <td>
+                            <router-link :to="{name:'command-preview', params:{id: command.id}}" class="control-action">
+                                View Command
+                            </router-link>
+                        </td>
+                    </tr>
 
-                <tr v-if="entry.tags.length">
-                    <td class="table-fit font-weight-bold">Tags</td>
-                    <td>
-                        <router-link
-                                v-for="tag in entry.tags"
-                                :key="tag"
-                                :to="{name:resource, query: {tag: tag}}"
-                                class="badge badge-info mr-1 font-weight-light">
-                            {{tag}}
-                        </router-link>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
+                    <tr v-if="entry.tags.length">
+                        <td class="table-fit font-weight-bold">Tags</td>
+                        <td>
+                            <router-link
+                                    v-for="tag in entry.tags"
+                                    :key="tag"
+                                    :to="{name:resource, query: {tag: tag}}"
+                                    class="badge badge-info mr-1 font-weight-light">
+                                {{tag}}
+                            </router-link>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
 
             <slot v-if="ready && entry" name="below-table" :entry="entry"></slot>
         </div>
