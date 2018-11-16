921954af4bb80c05b805e4fe32ec7c8337c354d0
Deleu <deleugyn@gmail.com>
2018-11-16 04:23:59 -0500

    Provide Migrate command for telescope-only

---
+++ src/Console/MigrateTelescopeCommand.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Config\Repository;
+use Illuminate\Console\Command;
+use Illuminate\Database\Migrations\Migrator;
+
+class MigrateTelescopeCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'telescope:migrate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create Telescope tables in a separate database without creating the app\'s tables.';
+
+    public function handle(Migrator $migrator, Repository $config)
+    {
+        $connection = $config->get('telescope.storage.database.connection');
+
+        $migrator->setConnection($connection);
+
+        $migrator->setOutput($this->output);
+
+        if (! $migrator->repositoryExists()) {
+            $migrator->getRepository()->createRepository();
+        }
+
+        $migrator->setOutput($this->output)->run([__DIR__.'/../Storage/migrations']);
+    }
+}
