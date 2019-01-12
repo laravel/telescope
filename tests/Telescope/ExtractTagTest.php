4860714353023d4993648217e7bd3cf3148b3e86
Jeremy Jass <jeremy@cleanhands-safehands.com>
2019-01-11 20:58:27 -0500

    test for mailables
    doesn't test the higher level MailWatcher, but tests the calls MailWatcher was making to ExtractTags that was causing problems before

--- tests/Telescope/ExtractTagTest.php
+++ tests/Telescope/ExtractTagTest.php
@@ -14,7 +14,6 @@ class ExtractTagTest extends FeatureTestCase
     public function test_extract_tag_from_array_containing_flat_collection()
     {
         $this->loadFactoriesUsing($this->app, __DIR__.'/../../src/Storage/factories');
-
         $flat_collection = factory(EntryModel::class, 1)->create();
 
         $tag = FormatModel::given($flat_collection->first());
@@ -29,12 +28,45 @@ class ExtractTagTest extends FeatureTestCase
     public function test_extract_tag_from array_containing_deep_collection()
     {
         $this->loadFactoriesUsing($this->app, __DIR__.'/../../src/Storage/factories');
-
         $deep_collection = factory(EntryModel::class, 1)->create()->groupBy('type');
 
         $tag = FormatModel::given($flat_collection->first()->first());
         $extracted_tag = ExtractTags::fromArray([$deep_collection]);
 
         $this->assertSame($tag, $extracted_tag);
+    }
+
+    /**
+     * @test
+     */
+    public function test_extract_tag_from_mailable()
+    {
+        $this->loadFactoriesUsing($this->app, __DIR__.'/../../src/Storage/factories');
+        $deep_collection = factory(EntryModel::class, 1)->create()->groupBy('type');
+        $mailable = new DummyMailableWithData($deep_collection);
+
+        $tag = FormatModel::given($flat_collection->first()->first());
+        $extracted_tag = ExtractTags::from($mailable);
+
+        $this->assertSame($tag, $extracted_tag);
+    }
+}
+
+class DummyMailableWithData extends Mailable
+{
+    private $mail_data;
+
+    public function __construct($mail_data) {
+        $this->mail_data = $mail_data;
+    }
+
+    public function build()
+    {
+        return $this->from('from@laravel.com')
+            ->to('to@laravel.com')
+            ->view(['raw' => 'simple text content'])
+            ->with([
+                'mail_data' => $this->mail_data,
+            ]);
     }
 }
