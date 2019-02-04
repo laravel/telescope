382b26f9dc29ce4c7a23563f2bef8282211e1be0
Tim Mortimer <tim@kiteframe.co.uk>
2019-02-04 20:37:50 +0000

    CS fixes

--- src/Storage/migrations/2018_08_08_100000_create_telescope_entries_table.php
+++ src/Storage/migrations/2018_08_08_100000_create_telescope_entries_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateTelescopeEntriesTable extends Migration
 {
@@ -32,7 +32,7 @@ class CreateTelescopeEntriesTable extends Migration
      */
     public function up()
     {
-        if (!app()->environment('self-testing') && !$this->currentConnectionIsConfiguredTelescopeConnection()) {
+        if (! app()->environment('self-testing') && ! $this->currentConnectionIsConfiguredTelescopeConnection()) {
             return;
         }
 
@@ -84,7 +84,7 @@ class CreateTelescopeEntriesTable extends Migration
     /**
      * Determine if the current connection is the configured Telescope connection.
      *
-     * @return boolean
+     * @return bool
      */
     protected function currentConnectionIsConfiguredTelescopeConnection()
     {
