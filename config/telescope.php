19702b7e9d52e968f4c77cb828262aed54b00545
Mathieu TUDISCO <oss@mathieutu.dev>
2019-04-03 13:56:33 +0200

    Change telescope connection default

--- config/telescope.php
+++ config/telescope.php
@@ -46,7 +46,7 @@ return [
 
     'storage' => [
         'database' => [
-            'connection' => env('DB_CONNECTION', 'mysql'),
+            'connection' => config('database.default'),
         ],
     ],
 
