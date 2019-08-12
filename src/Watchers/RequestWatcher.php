cbe84cc384db90f9ecfd65ddaf29a99645808e9e
Barry vd. Heuvel <barry@fruitcake.nl>
2019-08-12 17:04:56 +0200

    Add memory to request watcher

--- src/Watchers/RequestWatcher.php
+++ src/Watchers/RequestWatcher.php
@@ -52,6 +52,7 @@ class RequestWatcher extends Watcher
             'response_status' => $event->response->getStatusCode(),
             'response' => $this->response($event->response),
             'duration' => $startTime ? floor((microtime(true) - $startTime) * 1000) : null,
+            'memory' => round(memory_get_peak_usage(true) / 1024 / 1025, 1),
         ]));
     }
 
