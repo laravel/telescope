528309d1d82bcaae1fbb3966c74361e7d02df764
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2019-08-20 22:11:32 -0500

    Adding meta for disavowing robots
    Adding this meta tag may disavow regular indexing robots (google, yahoo, bing, etc.), to index the contents inside Telescope URLs.
    Take into account that this is not a barrier to get access to the Telescope components, but at least may avoid those sensitive sections to get index by mistake (there are some indexed already).
    Additionally, adding this to robots.txt may disavow robots for indexing, but will expose your Telescope URL to the public so you may want to avoid this and using this meta tag instead.

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,6 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="shortcut icon" href="{{ asset('/vendor/telescope/favicon.ico') }}">
+    
+    <!-- Disavowing all robots "access" -->
+    <meta name="robots" content="noindex, nofollow">
 
     <title>Telescope{{ config('app.name') ? ' - ' . config('app.name') : '' }}</title>
 
