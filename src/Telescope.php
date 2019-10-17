03574553bc05b8e1863e92321446c0cb4dcfe343
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2019-10-17 16:41:09 -0600

    wip

--- src/Telescope.php
+++ src/Telescope.php
@@ -183,7 +183,7 @@ class Telescope
                 'vendor/telescope*',
                 'horizon*',
                 'vendor/horizon*',
-                'nova-api*'
+                'nova-api*',
             ], config('telescope.ignore_paths', []))
         );
     }
