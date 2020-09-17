a857d133ac380cc2fc1c7247bf66988d9e663ba7
Senthil Vel <senthilvelpro@icloud.com>
2020-09-17 15:45:30 +0530

    Update PrunableRepository.php

--- src/Contracts/PrunableRepository.php
+++ src/Contracts/PrunableRepository.php
@@ -10,6 +10,7 @@ interface PrunableRepository
      * Prune all of the entries older than the given date.
      *
      * @param  \DateTimeInterface  $before
+     * @param  $keepExceptions
      * @return void
      */
     public function prune(DateTimeInterface $before, $keepExceptions);
