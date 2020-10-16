70530fe378021ba091ef2fc9ea0a89b712ff9fa8
Ali Shaikh <Ali-Shaikh@users.noreply.github.com>
2020-10-16 15:21:35 +0500

    [4.x] Determine if the incoming entry is a request, added isRequest() filter
    This will allow us to filter the entries of type 'request' in the register method in TelescopeServiceProvider

--- src/IncomingEntry.php
+++ src/IncomingEntry.php
@@ -193,6 +193,16 @@ class IncomingEntry
     }
 
     /**
+     * Determine if the incoming entry is a request.
+     *
+     * @return bool
+     */
+    public function isRequest()
+    {
+        return $this->type === EntryType::REQUEST;
+    }
+
+    /**
      * Determine if the incoming entry is a query.
      *
      * @return bool
