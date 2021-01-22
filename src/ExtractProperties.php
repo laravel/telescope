ea6c8db4f5e4e9da066439760af7010f858b9078
william <21337627+wfeller@users.noreply.github.com>
2021-01-22 17:13:17 +0100

    Prevent access to uninitialized typed properties

--- src/ExtractProperties.php
+++ src/ExtractProperties.php
@@ -20,6 +20,10 @@ class ExtractProperties
         return collect((new ReflectionClass($target))->getProperties())
             ->mapWithKeys(function ($property) use ($target) {
                 $property->setAccessible(true);
+                
+                if (PHP_VERSION_ID >= 70400 && ! $property->isInitialized($target)) {
+                    return [];
+                }
 
                 if (($value = $property->getValue($target)) instanceof Model) {
                     return [$property->getName() => FormatModel::given($value)];
