d0b01afc1314c404a08bd9d7fe09d0d16ee56b88
Michele Salvador <michele.salvador@absontheweb.com>
2021-02-01 22:01:58 +0100

    Improved phpdoc

--- src/Storage/EntryModel.php
+++ src/Storage/EntryModel.php
@@ -3,11 +3,16 @@
 namespace Laravel\Telescope\Storage;
 
 
-class EntryModel 
+class EntryModel
 {
+
+    /* Return the model according to the database in use
+     *
+     * @return MongoModel | RegularModel
+     */
+
     public static function getModelClass()
     {
         return env('DB_CONNECTION') === "mongodb" ? MongoModel::class : RegularModel::class;
     }
-
 }
