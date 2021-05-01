3fc2949b6c93dbaa90e9402645e3a9571430b23c
Lorrhan Lourenço <lorrhanlourenco@gmail.com>
2021-05-01 14:47:43 -0300

    add space

--- src/Storage/EntryModel.php
+++ src/Storage/EntryModel.php
@@ -117,7 +117,7 @@ class EntryModel extends Model
     {
         $query->when($options->tag, function ($query, $tag) {
             if(strpos($tag, ',') !== false) {
-                $tags = collect(explode(',', $tag))->filter(function($tag){
+                $tags = collect(explode(',', $tag))->filter(function ($tag) {
                     return strlen($tag) > 0; 
                 });
                 $query->select('entry_uuid')->from('telescope_entries_tags')->whereIn('tag', $tags);
