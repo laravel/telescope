db65855778586e66fd261d85348a85e5e31c3292
Taylor Otwell <taylorotwell@users.noreply.github.com>
2021-07-21 12:55:03 +0000

    Apply fixes from StyleCI

--- resources/js/mixins/entriesStyles.js
+++ resources/js/mixins/entriesStyles.js
@@ -42,7 +42,7 @@ export default {
         },
 
         requestStatusClass(status) {
-            if (! status) return 'danger';
+            if (!status) return 'danger';
             if (status < 300) return 'secondary';
             if (status < 400) return 'info';
             if (status < 500) return 'warning';
