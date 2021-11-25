abc96609553badb8907f749c95a408e6a72aab73
Taylor Otwell <taylor@laravel.com>
2021-11-24 20:14:54 -0600

    formatting

--- src/Watchers/RequestWatcher.php
+++ src/Watchers/RequestWatcher.php
@@ -36,7 +36,8 @@ class RequestWatcher extends Watcher
      */
     public function recordRequest(RequestHandled $event)
     {
-        if (! Telescope::isRecording() || $this->shouldIgnore($event)) {
+        if (! Telescope::isRecording() ||
+            $this->shouldIgnoreStatusCode($event)) {
             return;
         }
 
@@ -56,6 +57,20 @@ class RequestWatcher extends Watcher
             'duration' => $startTime ? floor((microtime(true) - $startTime) * 1000) : null,
             'memory' => round(memory_get_peak_usage(true) / 1024 / 1024, 1),
         ]));
+    }
+
+    /**
+     * Determine if the request should be ignored based on its status code.
+     *
+     * @param  mixed  $event
+     * @return bool
+     */
+    protected function shouldIgnoreStatusCode($event)
+    {
+        return in_array(
+            $event->response->getStatusCode(),
+            $this->options['ignore_status_codes'] ?? []
+        );
     }
 
     /**
@@ -207,16 +222,5 @@ class RequestWatcher extends Watcher
                 return json_decode(json_encode($value), true);
             }
         })->toArray();
-    }
-
-    /**
-     * Determine if the request should be ignored.
-     *
-     * @param  mixed  $event
-     * @return bool
-     */
-    private function shouldIgnore($event)
-    {
-        return in_array($event->response->getStatusCode(), $this->options['ignore_status_code'] ?? []);
     }
 }
