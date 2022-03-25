e8f09dc2419810c328b3c17cc01bf74b2752d5fd
Tom Janssen <dododedodonl@users.noreply.github.com>
2022-03-25 16:08:29 +0100

    CacheWatcher hide values (#1206)
    * add ability to hide a cache value
    
    * add tests to confirm hiding
    
    * fix style
    
    * formatting
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Watchers/CacheWatcher.php
+++ src/Watchers/CacheWatcher.php
@@ -42,7 +42,7 @@ class CacheWatcher extends Watcher
         Telescope::recordCache(IncomingEntry::make([
             'type' => 'hit',
             'key' => $event->key,
-            'value' => $event->value,
+            'value' => $this->formatValue($event),
         ]));
     }
 
@@ -79,7 +79,7 @@ class CacheWatcher extends Watcher
         Telescope::recordCache(IncomingEntry::make([
             'type' => 'set',
             'key' => $event->key,
-            'value' => $event->value,
+            'value' => $this->formatValue($event),
             'expiration' => $this->formatExpiration($event),
         ]));
     }
@@ -100,6 +100,33 @@ class CacheWatcher extends Watcher
             'type' => 'forget',
             'key' => $event->key,
         ]));
+    }
+
+    /**
+     * Determine the value of an event.
+     *
+     * @param  mixed  $event
+     * @return mixed
+     */
+    private function formatValue($event)
+    {
+        return (! $this->shouldHideValue($event))
+                    ? $event->value
+                    : '********';
+    }
+
+    /**
+     * Determine if the event value should be ignored.
+     *
+     * @param  mixed  $event
+     * @return bool
+     */
+    private function shouldHideValue($event)
+    {
+        return Str::is(
+            $this->options['hidden'] ?? [],
+            $event->key
+        );
     }
 
     /**
