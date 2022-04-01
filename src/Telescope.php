88e3ded3e1fc301a82403820b2f68ec5a9e092e3
Taylor Otwell <taylor@laravel.com>
2022-04-01 09:27:12 -0500

    wip (#1215)

--- src/Telescope.php
+++ src/Telescope.php
@@ -146,7 +146,7 @@ class Telescope
             (static::runningApprovedArtisanCommand($app) ||
             static::handlingApprovedRequest($app))
         ) {
-            static::startRecording();
+            static::startRecording($loadMonitoredTags = false);
         }
     }
 
@@ -246,11 +246,14 @@ class Telescope
     /**
      * Start recording entries.
      *
+     * @param  bool  $loadMonitoredTags
      * @return void
      */
-    public static function startRecording()
+    public static function startRecording($loadMonitoredTags = true)
     {
-        app(EntriesRepository::class)->loadMonitoredTags();
+        if ($loadMonitoredTags) {
+            app(EntriesRepository::class)->loadMonitoredTags();
+        }
 
         static::$shouldRecord = ! cache('telescope:pause-recording');
     }
