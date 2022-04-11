81ae2d8161d8c484ae1366d2c8e31702512c4e7a
Connor Forsyth <connor@randomstate.co.uk>
2022-04-11 15:14:29 +0100

    Relax user type in GateWatcher to match Laravel base framework.

--- src/Watchers/GateWatcher.php
+++ src/Watchers/GateWatcher.php
@@ -40,13 +40,13 @@ class GateWatcher extends Watcher
     /**
      * Record a gate check.
      *
-     * @param  \Illuminate\Contracts\Auth\Authenticatable|null  $user
+     * @param  \Illuminate\Contracts\Auth\Authenticatable|mixed|null  $user
      * @param  string  $ability
      * @param  bool  $result
      * @param  array  $arguments
      * @return bool
      */
-    public function recordGateCheck(?Authenticatable $user, $ability, $result, $arguments)
+    public function recordGateCheck($user, $ability, $result, $arguments)
     {
         if (! Telescope::isRecording() || $this->shouldIgnore($ability)) {
             return;
