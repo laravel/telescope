92353f423cc7b2d2d7216b78e57e804a477ffa7f
Aidan <aidanartherton@gmail.com>
2023-01-29 20:40:32 -0500

    Cleanup after previous solution removed

--- src/Watchers/DumpWatcher.php
+++ src/Watchers/DumpWatcher.php
@@ -41,9 +41,7 @@ class DumpWatcher extends Watcher
     public function register($app)
     {
         try {
-            $dumpWatcherCache = $this->cache->get('telescope:dump-watcher');
-
-            if (! ($this->options['always'] ?? false) && ! $dumpWatcherCache) {
+            if (! ($this->options['always'] ?? false) && ! $this->cache->get('telescope:dump-watcher')) {
                 return;
             }
         } catch (\Exception) {
