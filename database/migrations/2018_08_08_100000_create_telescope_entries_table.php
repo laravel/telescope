2a3fd4643775650190d54d667037708f7e336be1
Kirill Smirnov <smirnovkl@uralsib.ru>
2023-02-16 16:01:49 +0300

    Fix migration bug in tests

--- database/migrations/2018_08_08_100000_create_telescope_entries_table.php
+++ database/migrations/2018_08_08_100000_create_telescope_entries_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateTelescopeEntriesTable extends Migration
 {
     /**
      * The database schema.
