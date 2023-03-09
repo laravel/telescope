1e5bbdd5eb49d0bc692b657bc5e30374a14ef704
Igor Finagin <Igor@Finag.in>
2023-03-09 11:00:37 +0600

    [4.x] Record entries after dd or exit

--- src/Watchers/DumpWatcher.php
+++ src/Watchers/DumpWatcher.php
@@ -4,8 +4,12 @@ namespace Laravel\Telescope\Watchers;
 
 use Exception;
 use Illuminate\Contracts\Cache\Factory as CacheFactory;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Foundation\Console\CliDumper as BaseCliDumper;
+use Illuminate\Foundation\Http\HtmlDumper as BaseHtmlDumper;
 use Laravel\Telescope\IncomingDumpEntry;
 use Laravel\Telescope\Telescope;
+use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\VarDumper\Cloner\VarCloner;
 use Symfony\Component\VarDumper\Dumper\HtmlDumper;
 use Symfony\Component\VarDumper\VarDumper;
@@ -14,16 +18,12 @@ class DumpWatcher extends Watcher
 {
     /**
      * The cache factory implementation.
-     *
-     * @var \Illuminate\Contracts\Cache\Factory
      */
-    protected $cache;
+    protected CacheFactory $cache;
 
     /**
      * Create a new watcher instance.
      *
-     * @param  \Illuminate\Contracts\Cache\Factory  $cache
-     * @param  array  $options
      * @return void
      */
     public function __construct(CacheFactory $cache, array $options = [])
@@ -55,21 +55,48 @@ class DumpWatcher extends Watcher
 
         $htmlDumper = new HtmlDumper();
         $htmlDumper->setDumpHeader('');
+        $baseDumper = static::getBaseDumper($app);
 
-        VarDumper::setHandler(function ($var) use ($htmlDumper) {
-            $this->recordDump($htmlDumper->dump(
-                (new VarCloner)->cloneVar($var), true
-            ));
+        VarDumper::setHandler(function ($var) use ($htmlDumper, $baseDumper) {
+            $dumper = (! $isNotDumpAndDie = static::isNotDumpAndDie())
+                ? $baseDumper
+                : $htmlDumper;
+
+            $dump = $dumper->dump((new VarCloner)->cloneVar($var), $isNotDumpAndDie);
+
+            if ($isNotDumpAndDie) {
+                $this->recordDump($dump);
+            }
         });
     }
 
     /**
-     * Record a dumped variable.
-     *
-     * @param  string  $dump
-     * @return void
+     * Get the base dumper instance.
      */
-    public function recordDump($dump)
+    protected static function getBaseDumper(Application $app): BaseCliDumper|BaseHtmlDumper
+    {
+        $baseDumperArgs = [$app->basePath(), $app['config']->get('view.compiled')];
+
+        return in_array(PHP_SAPI, ['cli', 'phpdbg'])
+            ? new BaseCliDumper(new ConsoleOutput, ...$baseDumperArgs)
+            : new BaseHtmlDumper(...$baseDumperArgs);
+    }
+
+    /**
+     * Determine if the current dump is not a dd() call.
+     */
+    protected static function isNotDumpAndDie(): bool
+    {
+        return collect(debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS))
+            ->pluck('function')
+            ->filter(fn ($value) => $value === 'dd')
+            ->isEmpty();
+    }
+
+    /**
+     * Record a dumped variable.
+     */
+    public function recordDump(string $dump): void
     {
         Telescope::recordDump(
             IncomingDumpEntry::make(['dump' => $dump])
