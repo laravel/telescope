be34f93161b133a667790084a8ebb5755168a517
Diogo Gomes <diogogomeswww@gmail.com>
2023-06-02 11:08:42 +0100

    convert to a pretty json string

--- resources/js/components/CustomVueJsonPretty.vue
+++ resources/js/components/CustomVueJsonPretty.vue
@@ -26,7 +26,7 @@ export default {
             if (this.data instanceof String) {
                 return this.$data;
             }
-            return JSON.stringify(this.data);
+            return JSON.stringify(this.data, null, '\t');
         }
     }
 }
@@ -38,7 +38,12 @@ export default {
             <span v-if="copying">Copied...</span>
             <copy-to-clipboard :text="copyText" @copy="handleCopy">
                 <a href="#">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M19 21H8V7h11m0-2H8a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h11a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2m-3-4H4a2 2 0 0 0-2 2v14h2V3h12V1z"/></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em"
+                         style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);"
+                         preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
+                        <path
+                            d="M19 21H8V7h11m0-2H8a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h11a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2m-3-4H4a2 2 0 0 0-2 2v14h2V3h12V1z"/>
+                    </svg>
                 </a>
             </copy-to-clipboard>
         </div>
