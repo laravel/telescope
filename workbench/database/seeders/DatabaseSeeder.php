a1e59b48d04125c7d0c673bea9a622be8f6dfbdb
Mior Muhammad Zaki <crynobone@gmail.com>
2023-08-07 15:29:43 +0800

    Telescope Workbench
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ workbench/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Workbench\Database\Seeders;
+
+use Database\Factories\UserFactory;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        $this->call(UserTableSeeder::class);
+    }
+}
