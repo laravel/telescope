c3a7e85ca4827acf1bf6fa27928b3c2d7da724ff
Gene Surov <esurov@gmail.com>
2023-10-14 12:10:46 +0200

    StyleCI fixes.

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
-import {defineConfig} from 'vite'
-import laravel from 'laravel-vite-plugin'
+import {defineConfig} from 'vite';
+import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue2';
 
 export default defineConfig({
     plugins: [
-        laravel([
-            'resources/sass/app.scss',
-            'resources/sass/app-dark.scss',
-            'resources/js/app.js',
-        ]),
+        laravel(['resources/sass/app.scss', 'resources/sass/app-dark.scss', 'resources/js/app.js']),
         vue({
             template: {
                 transformAssetUrls: {
