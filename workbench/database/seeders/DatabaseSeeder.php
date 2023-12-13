c71046a8a1794dcfc128151a50321be03cee8a68
Nuno Maduro <enunomaduro@gmail.com>
2023-12-13 09:16:29 +0000

    Fixes test suite

--- workbench/database/seeders/DatabaseSeeder.php
+++ workbench/database/seeders/DatabaseSeeder.php
@@ -13,14 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $password = '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'; // password
-
-        User::forceCreate([
-            'name' => 'Taylor Otwell',
-            'email' => 'taylor@laravel.com',
-            'password' => $password,
-        ]);
-
-        UserFactory::new()->times(9)->create();
+        //
     }
 }
