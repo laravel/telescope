c3a794ae6e011c8410e4de7c49ddab079ab846b0
Amirsobhan Nafariyeh <amirsobhan1553@gmail.com>
2024-08-20 18:24:41 +0330

    [5.x] Enhance Telescope Clear Method (#1507)
    * improve the `clear` method in `DatabaseEntriesRepository.php`
    
    * improve the `clear` method in `DatabaseEntriesRepository.php`
    
    * improve the `clear` method in `DatabaseEntriesRepository.php`
    
    * wip
    
    * wip
    
    * Update DatabaseEntriesRepository.php
    
    ---------
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Storage/DatabaseEntriesRepository.php
+++ src/Storage/DatabaseEntriesRepository.php
@@ -3,9 +3,11 @@
 namespace Laravel\Telescope\Storage;
 
 use DateTimeInterface;
+use Illuminate\Database\QueryException;
 use Illuminate\Database\UniqueConstraintViolationException;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 use Laravel\Telescope\Contracts\ClearableRepository;
 use Laravel\Telescope\Contracts\EntriesRepository as Contract;
 use Laravel\Telescope\Contracts\PrunableRepository;
@@ -378,13 +380,22 @@ class DatabaseEntriesRepository implements Contract, ClearableRepository, Prunab
      */
     public function clear()
     {
-        do {
-            $deleted = $this->table('telescope_entries')->take($this->chunkSize)->delete();
-        } while ($deleted !== 0);
+        try {
+            Schema::disableForeignKeyConstraints();
 
-        do {
-            $deleted = $this->table('telescope_monitoring')->take($this->chunkSize)->delete();
-        } while ($deleted !== 0);
+            $this->table('telescope_entries')->truncate();
+            $this->table('telescope_monitoring')->truncate();
+        } catch (QueryException) {
+            do {
+                $deleted = $this->table('telescope_entries')->take($this->chunkSize)->delete();
+            } while ($deleted !== 0);
+
+            do {
+                $deleted = $this->table('telescope_monitoring')->take($this->chunkSize)->delete();
+            } while ($deleted !== 0);
+        } finally {
+            Schema::enableForeignKeyConstraints();
+        }
     }
 
     /**
