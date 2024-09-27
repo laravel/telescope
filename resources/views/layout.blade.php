82f2e430aac37edd02593ff4f997d9fc9270784a
Zakaria AKTOUF <101515566+zackAJ@users.noreply.github.com>
2024-09-27 23:04:48 +0100

    Fix removed extra margin

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -101,7 +101,7 @@
                     </li>
 
 
-                    <li class="nav-item mt-3">
+                    <li class="nav-item">
                         <router-link active-class="active" to="/batches" class="nav-link d-flex align-items-center">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                 <path fill-rule="evenodd" d="M2 3.75A.75.75 0 012.75 3h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 3.75zm0 4.167a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75a.75.75 0 01-.75-.75zm0 4.166a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75a.75.75 0 01-.75-.75zm0 4.167a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75a.75.75 0 01-.75-.75z" clip-rule="evenodd" />
