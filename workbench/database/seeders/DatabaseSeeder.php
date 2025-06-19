12aaca419cd1e3b03abb30b28490af614a8bf54e
Mior Muhammad Zaki <crynobone@gmail.com>
2025-06-19 20:01:30 +0800

    Improves code preview
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- workbench/database/seeders/DatabaseSeeder.php
+++ workbench/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Workbench\Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Foundation\Auth\User;
+use Orchestra\Testbench\Factories\UserFactory;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $password = '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'; // password
+
+        User::forceCreate([
+            'name' => 'Laravel User',
+            'email' => 'test@example.com',
+            'password' => $password,
+        ]);
+
+        UserFactory::new()->times(9)->create();
     }
 }
