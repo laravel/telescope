40ff743ed823adf833f3bbada87ec9b4fc70d7d0
Wes Hooper <weshooper@users.noreply.github.com>
2025-08-16 01:49:21 +0100

    [5.x] Removed redundant default values from config file
    PhpStorm Inspector pointed this out.
    
    Similar to previous @ https://github.com/laravel/telescope/pull/1323

--- config/telescope.php
+++ config/telescope.php
@@ -76,8 +76,8 @@ return [
     */
 
     'queue' => [
-        'connection' => env('TELESCOPE_QUEUE_CONNECTION', null),
-        'queue' => env('TELESCOPE_QUEUE', null),
+        'connection' => env('TELESCOPE_QUEUE_CONNECTION'),
+        'queue' => env('TELESCOPE_QUEUE'),
         'delay' => env('TELESCOPE_QUEUE_DELAY', 10),
     ],
 
