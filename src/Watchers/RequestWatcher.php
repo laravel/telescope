674f5e162770cc8b9e0e9f33931bf8e33cbd2746
Utkarsh Rokade <utkarshrokade@gmail.com>
2025-10-30 18:03:16 +0530

    feat: update shouldIgnoreUriPath method to use config for ignored paths during tests
    * Update RequestWatcher.php
    * Update RequestWatchersTest.php

--- src/Watchers/RequestWatcher.php
+++ src/Watchers/RequestWatcher.php
@@ -94,18 +94,28 @@ class RequestWatcher extends Watcher
 
     /**
      * Determine if the request should be ignored based on its path.
-     * 
-     * @param mixed $event
+     *
+     * @param  mixed  $event
      * @return bool
      */
     protected function shouldIgnoreUriPath($event)
     {
-        $ignoredPaths = $this->options['ignore_uri_paths'] ?? [];
-        
+        // $ignoredPaths = $this->options['ignore_uri_paths'] ?? [];
+        /**
+         * Used config() to pass test cases instead of $this->options.
+         * During unit tests, $this->options fails because:
+         * 1. getEnvironmentSetUp boots Telescope with default (empty) config.
+         * 2. RequestWatcher is initialized with these empty options.
+         * 3. Later config changes via $this->app->get('config')->set(...) are too late—
+         *    the RequestWatcher still uses the old config.
+         */
+        $config = config('telescope.watchers.'.static::class);
+        $ignoredPaths = $config['ignore_uri_paths'] ?? [];
+
         if (empty($ignoredPaths)) {
             return false;
         }
-        
+
         return collect($ignoredPaths)->contains(function ($path) use ($event) {
             return $event->request->is(ltrim($path, '/'));
         });
