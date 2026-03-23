bc7e3fdc8e5ee0a9a3329c1d2d0954a1cb1f9ab7
Bartłomiej Gajda <b.gajda@webard.me>
2026-03-23 16:05:10 +0100

    style

--- src/TelescopeServiceProvider.php
+++ src/TelescopeServiceProvider.php
@@ -161,11 +161,11 @@ class TelescopeServiceProvider extends ServiceProvider
 
         $this->app->when(DatabaseEntriesRepository::class)
             ->needs('$connection')
-            ->give(fn() => config('telescope.storage.database.connection'));
+            ->give(fn () => config('telescope.storage.database.connection'));
 
         $this->app->when(DatabaseEntriesRepository::class)
             ->needs('$chunkSize')
-            ->give(fn() => config('telescope.storage.database.chunk'));
+            ->give(fn () => config('telescope.storage.database.chunk'));
     }
 
     /**
