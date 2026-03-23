9886c92fb4e16b087feee83f8312b43ad15bcb7b
Bartłomiej Gajda <b.gajda@webard.me>
2026-03-23 16:01:38 +0100

    fix

--- src/TelescopeServiceProvider.php
+++ src/TelescopeServiceProvider.php
@@ -161,11 +161,11 @@ class TelescopeServiceProvider extends ServiceProvider
 
         $this->app->when(DatabaseEntriesRepository::class)
             ->needs('$connection')
-            ->give(config('telescope.storage.database.connection'));
+            ->give(fn() => config('telescope.storage.database.connection'));
 
         $this->app->when(DatabaseEntriesRepository::class)
             ->needs('$chunkSize')
-            ->give(config('telescope.storage.database.chunk'));
+            ->give(fn() => config('telescope.storage.database.chunk'));
     }
 
     /**
